ba95b7ec8fa809f8efb5e66954508e4dbcf679b0
Frank Rodriguez <frank0631@gmail.com>
2018-07-18 03:00:32 +0000

    add findByTitleContaining

--- src/main/java/com/frank0631/huginn/book/BookRepository.java
+++ src/main/java/com/frank0631/huginn/book/BookRepository.java
@@ -13,4 +13,6 @@ import java.util.List;
 public interface BookRepository extends CrudRepository<BookEntity, Long>, BookRepositoryCustom {
 
     List<BookEntity> findByTitle(@Param("title")String title);
+    
+    List<BookEntity> findByTitleContaining(@Param("title")String title);
 }
